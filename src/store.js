1ddd310ab62988f85310a60d63314d2d9a8f7b83
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-04-14 14:20:04 +0500

    middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = () => (dispatch) => (action) => {
+  console.log(action.type);
+  return dispatch(action);
+};
+
+const stringMiddleware = () => (dispatch) => (action) => {
+  if (typeof action === 'string') {
+    return dispatch({
+      type: action,
+    });
+  }
+
+  return dispatch(action);
+}
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
